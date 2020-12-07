46b2b4c7644885a10028f47075a3959be172c592
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2020-12-07 18:52:34 -0300

    Desenvolvimento da funcionalidade de atualização to Tópico no REST.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 //import java.util.Arrays;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 //import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
@@ -82,4 +85,15 @@ public class TopicosController {
 		Topico topico = topicoRepository.getOne(id);
 		return new DetalhesDoTopicoDto(topico);
 	}
+	
+	//@Transactional -> Avisar o Spring para realizar o commit no final da transação.
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+		Topico topico = form.atualizar(id, topicoRepository);
+		//return ResponseEntity.ok().body(new TopicoDto(topico));
+		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
+	
 }
