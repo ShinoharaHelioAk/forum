224fd69de9e1f0de4cd5e7f40b4c1d0a2bfcf3c2
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2020-12-08 12:53:06 -0300

    Parte 2: Adição da ordenação na API de listagem de Tópicos.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -55,14 +56,15 @@ public class TopicosController {
 	//@RequestParam -> Diz para o Spring que são parâmetros de url, e eles se tornam obrigatórios para serem informados.
 	//@ResponseBody //Indicamos que o retorno do método deve ser serializado e devolvido no corpo da resposta.
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
-			@RequestParam int pagina, @RequestParam int qtd) {
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao) {
+	//public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd) {
 	//public List<TopicoDto> lista(String nomeCurso) {
 		
 //		Topico topico = new Topico("Duvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
 //		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 		
-		Pageable paginacao = PageRequest.of(pagina, qtd);
+		//Pageable paginacao = PageRequest.of(pagina, qtd);
+		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC /*Direction.DESC*/, ordenacao);
 		
 		//List<Topico> topicos = new ArrayList<Topico>();
 		Page<Topico> topicos = null;
