ddbd0d8240b5f721102497e0bda286319bbf4f18
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2020-12-08 14:19:22 -0300

    Parte 2: Simplificando a paginação e ordenação da listagem de Tópicos na API REST.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,9 +11,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+//import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -56,7 +58,10 @@ public class TopicosController {
 	//@RequestParam -> Diz para o Spring que são parâmetros de url, e eles se tornam obrigatórios para serem informados.
 	//@ResponseBody //Indicamos que o retorno do método deve ser serializado e devolvido no corpo da resposta.
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao) {
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso 
+			//, @RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao
+			, @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao
+			) {
 	//public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd) {
 	//public List<TopicoDto> lista(String nomeCurso) {
 		
@@ -64,7 +69,8 @@ public class TopicosController {
 //		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 		
 		//Pageable paginacao = PageRequest.of(pagina, qtd);
-		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC /*Direction.DESC*/, ordenacao);
+		//Direction.DESC
+		//Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
 		
 		//List<Topico> topicos = new ArrayList<Topico>();
 		Page<Topico> topicos = null;
