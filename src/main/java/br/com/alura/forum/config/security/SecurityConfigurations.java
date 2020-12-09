f670be148abdfb1287ac82d9a577144e1a864fcf
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2020-12-08 23:30:46 -0300

    Parte 2: Implementação da autenticação do usuário, para acessar os Endpoints privados.

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,22 +8,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
-public class SecutiryConfigurations extends WebSecurityConfigurerAdapter {
-	
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private AutenticacaoService autenticacaoService;
 	
 	
 	//Método que serve para configurar a parte de autenticação.
 	//A parte de controle de acesso, de login, fica nesse método.
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
-	
+	/*public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("123456"));
+	}*/
 	
 	
 	//Método que serve para fazer configurações de autorização.
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$gHLZqtQW5Is5pztQ0WYaL.VzC3X/eMFlfhobPQWPt7GMokt/ma2aS');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
