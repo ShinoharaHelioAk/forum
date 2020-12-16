544bef9fc806a5675ad1523987bacf13f99fc43d
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2020-12-16 18:41:07 -0300

    Correção da implementação dos testes unitários.

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -25,18 +25,20 @@ public class CursoRepositoryTest {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
-	@Autowired
-	private TestEntityManager em;
+	/*@Autowired
+	private TestEntityManager em;*/
 
 	@Test
 	public void findByNomeTest01() {
 		String nomeCurso = "HTML 5";
 		
-		Curso html5 = new Curso();
+		/*Curso html5 = new Curso();
 		html5.setNome(nomeCurso);
-		em.persist(html5);
+		html5.setCategoria("Programação");
+		em.persist(html5);*/
 		
 		Curso curso = cursoRepository.findByNome(nomeCurso);
+		
 		Assert.assertNotNull(curso);
 		Assert.assertEquals(nomeCurso, curso.getNome());
 	}
