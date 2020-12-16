5f83f4c8dd986d68dd541b99f41dbf6d1d49da7c
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2020-12-16 10:07:16 -0300

    Parte 3: Testando um Repository.

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -4,23 +4,36 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.alura.forum.modelo.Curso;
 
 //@DataJpaTest -> Anotação específica para execução de testes automatizados com interfaces Repository.
+//@ActiveProfiles("test") -> Força o Spring para que esta classe de testes tenha um Profile declarado como Ativo.
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 	@Autowired
 	private CursoRepository cursoRepository;
+	
+	@Autowired
+	private TestEntityManager em;
 
 	@Test
 	public void findByNomeTest01() {
 		String nomeCurso = "HTML 5";
-		Curso curso = cursoRepository.findByNome(nomeCurso);
 		
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		em.persist(html5);
+		
+		Curso curso = cursoRepository.findByNome(nomeCurso);
 		Assert.assertNotNull(curso);
 		Assert.assertEquals(nomeCurso, curso.getNome());
 	}
